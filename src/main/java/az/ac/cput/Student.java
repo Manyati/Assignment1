6b20f646297f75ed622f78ae49a5965fcb4f3a0e
Manyati-2020 <manymunya@gmail.com>
2021-04-17 22:58:51 +0200

    215Branch

---
+++ src/main/java/az/ac/cput/Student.java
@@ -0,0 +1,41 @@
+package az.ac.cput;
+
+import java.util.Objects;
+
+public class Student {
+    private String id,name;
+
+    public String getId() {
+        return id;
+    }
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id.equals(student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/az/ac/cput/StudentTest.java
@@ -0,0 +1,23 @@
+package az.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/*@Manyati 215211855
+*
+ */
+
+class StudentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @Disabled
+    void testIgnore(){
+        System.out.println("You can Ignore this! ");
+    }
+}
