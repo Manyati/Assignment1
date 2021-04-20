87df574a756b036781fe861919c5fe32020d71d4
Honest Mpungu <mpunguhonest@gmail.com>
2021-04-20 17:58:17 +0200

    Committing Assignmnet Practice

---
+++ src/main/java/az/ac/cput/Game.java
@@ -0,0 +1,18 @@
+package az.ac.cput;
+
+
+/**
+ * Honest Mpungu!
+ * Student Number 215072081
+ * Part-Time group
+ * Group AssignmentG
+ */
+
+public class Game {
+    public String play(int number) {
+        return String.valueOf(number);
+    }
+    public long addTwoNumbers() {
+        return 0;
+    }
+}
