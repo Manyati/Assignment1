e9661e236213692204c04537e0c31e6ff71f89ba
anongxa <61239936+anongxa@users.noreply.github.com>
2021-04-21 14:41:30 +0200

    made changes

--- src/test/java/az/ac/cput/BookTest.java
+++ src/test/java/az/ac/cput/BookTest.java
@@ -1,7 +1,13 @@
 package az.ac.cput;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
+
+    @Test
+    void testEquality(){
+        assertEquals(1,1);
+    }
 
 }
